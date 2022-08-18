4f1083622a1f9fbe8623d3f91ef8a38868f5c509
Owen Craig <owencraig1999@gmail.com>
2022-08-18 17:03:12 +0100

    Add project files.

---
+++ FixedStackArrayCMake/FixedStackArrayCMake.cpp
@@ -0,0 +1,12 @@
+﻿// FixedStackArrayCMake.cpp : Defines the entry point for the application.
+//
+
+#include "FixedStackArrayCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
